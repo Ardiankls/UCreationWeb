699671bb9d9d1a97f61c958eeb9a58cf052fb09a
Fadheljanuar <62705800+Fadheljanuar@users.noreply.github.com>
2021-01-22 00:18:46 +0700

    blade

--- resources/views/staff/profile/detail.blade.php
+++ resources/views/staff/profile/detail.blade.php
@@ -20,19 +20,10 @@
         }
     </style>
     <div class="container-fluid " >
-        <div class="row">
-            <div class="col-sm-4 shadow p-5 m-5 rounded-3" style="background-color: white; color: #979BFF;">
-                <div class="row" style=" height: 100%">
-                    <h1 class="text-center " style=" font-size: 8em">5</h1>
-                    <h2 class="text-center mx-auto" style="">Total Project</h2>
-                </div>
-            </div>
-            <div class="col-sm-6 shadow p-5 m-5 rounded-3" style="background-color: white; color: #979BFF">
+            <div class="col-sm-11  shadow p-5 m-5 rounded-3" style="background-color: white; color: #979BFF">
                 <div class="row">
                     <div class="col-sm-5" style="">
-                        <picture >
-                            <img src="img/blankprofile.png" class="img-fluid rounded-3 shadow-sm">
-                        </picture>
+
                         {{--                    <img  class=" shadow rounded-3 "  src="img/blankprofile.png" style="width: 4em; height: 4em; margin-top: -4em; margin-right: 1em" >--}}
                     </div>
                     <div class="col-sm-7" style="color: #979BFF">
