18fad6a11aaa19f17d6ad1fb4e076f98348c2539
Ardian-Kurniawan <ardiankurniawan7@gmail.com>
2021-01-22 02:17:28 +0700

    dsad

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -19,10 +19,9 @@
                 @foreach($creation as $creation)
                     <div class="col-lg-3">
                         @csrf
-                        {{asset('image/creation/'.$creation->picture)}}
-                     <a class="" href="{{asset('image/creation/'.$creation->picture)}}">
+                     <a class="" href="{{ route('admin.creation.show', $creation->id) }}">
                 <div class="card-view bg-white shadow "  style="width: 200px; height: 267px; margin-top: 50px; border-radius: 15px;">
-                    <img src="/image/creation/{{$creation->picture}}" class="card-img-top" style="padding: 2px ;margin: 0 auto; height: 155px; margin-top: 10px; border-radius: 15px;">
+                    <img src="{{asset('image/creation/'.$creation->picture)}}" class="card-img-top" style="padding: 2px ;margin: 0 auto; height: 155px; margin-top: 10px; border-radius: 15px;">
                     <h1 class="title mx-auto text-center p-1 fs-5 font-weight-bold" style="color: #979BFF">{{$creation->name}}</h1>
                     <p class="proj-shortdesc mx-auto text-center     p-2 fs-6" style="color: #979BFF"> {{$creation->short_description}}</p>
                 </div>
